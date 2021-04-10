99cb9854c75afd144b071f95bd1885c8e3389495
Arsh Saxena <66147853+arshsaxena@users.noreply.github.com>
2021-04-10 20:15:18 +0530

    built app

---
+++ app/src/main/java/com/arshsaxena/github/io/MainActivity.java
@@ -0,0 +1,67 @@
+package com.arshsaxena.github.io;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://arshsaxena.github.io");
+        myWebView.setWebViewClient(new WebViewClient());
+        if (savedInstanceState != null)
+        {  ((WebView) findViewById(R.id.webView)).restoreState(savedInstanceState.getBundle("webViewState"));}
+        else {
+            myWebView = (WebView)findViewById(R.id.webView);
+            load();
+        }
+    }
+
+    public void load() {
+        myWebView=(WebView)findViewById(R.id.webView);
+        myWebView.setWebViewClient(new MyBrowser());
+        myWebView.getSettings().setLoadsImagesAutomatically(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        myWebView.loadUrl("https://arshsaxena.github.io");
+    }
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Bundle bundle = new Bundle();
+        myWebView.saveState(bundle);
+        outState.putBundle("webViewState", bundle);
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle state) {
+        super.onRestoreInstanceState(state);
+        Bundle bundle = new Bundle();
+        myWebView.saveState(bundle);
+        state.putBundle("webViewState", bundle);
+    }
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
