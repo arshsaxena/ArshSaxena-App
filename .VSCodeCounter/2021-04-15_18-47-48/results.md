1c131a2c10bf4d6212ac6c8ecbe1e7e1609d59c9
Arsh Saxena <arsh.saxena02@gmail.com>
2021-04-15 18:51:35 +0530

    added: error page and intent

---
+++ .VSCodeCounter/2021-04-15_18-47-48/results.md
@@ -0,0 +1,62 @@
+# Summary
+
+Date : 2021-04-15 18:47:48
+
+Directory c:\Users\sande\Desktop\iArsh\iDocuments\apps\ArshSaxena
+
+Total : 35 files,  719 codes, 42 comments, 85 blanks, all 846 lines
+
+[details](details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| XML | 17 | 278 | 10 | 5 | 293 |
+| Java | 5 | 155 | 12 | 34 | 201 |
+| CSS | 2 | 67 | 0 | 1 | 68 |
+| Batch | 1 | 61 | 0 | 24 | 85 |
+| Groovy | 3 | 58 | 3 | 9 | 70 |
+| JSON | 2 | 57 | 0 | 0 | 57 |
+| HTML | 1 | 24 | 0 | 2 | 26 |
+| Markdown | 2 | 11 | 0 | 9 | 20 |
+| Properties | 2 | 8 | 17 | 1 | 26 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 35 | 719 | 42 | 85 | 846 |
+| .idea | 4 | 63 | 0 | 0 | 63 |
+| app | 25 | 564 | 22 | 56 | 642 |
+| app\release | 1 | 18 | 0 | 0 | 18 |
+| app\src | 22 | 470 | 22 | 50 | 542 |
+| app\src\androidTest | 1 | 15 | 6 | 5 | 26 |
+| app\src\androidTest\java | 1 | 15 | 6 | 5 | 26 |
+| app\src\androidTest\java\com | 1 | 15 | 6 | 5 | 26 |
+| app\src\androidTest\java\com\arshsaxena | 1 | 15 | 6 | 5 | 26 |
+| app\src\androidTest\java\com\arshsaxena\github | 1 | 15 | 6 | 5 | 26 |
+| app\src\androidTest\java\com\arshsaxena\github\io | 1 | 15 | 6 | 5 | 26 |
+| app\src\main | 20 | 446 | 11 | 42 | 499 |
+| app\src\main\assets | 4 | 100 | 0 | 11 | 111 |
+| app\src\main\assets\fonts | 1 | 9 | 0 | 8 | 17 |
+| app\src\main\java | 3 | 131 | 1 | 26 | 158 |
+| app\src\main\java\com | 3 | 131 | 1 | 26 | 158 |
+| app\src\main\java\com\arshsaxena | 3 | 131 | 1 | 26 | 158 |
+| app\src\main\java\com\arshsaxena\github | 3 | 131 | 1 | 26 | 158 |
+| app\src\main\java\com\arshsaxena\github\io | 3 | 131 | 1 | 26 | 158 |
+| app\src\main\res | 12 | 178 | 10 | 3 | 191 |
+| app\src\main\res\drawable | 1 | 74 | 0 | 1 | 75 |
+| app\src\main\res\drawable-v24 | 1 | 30 | 0 | 0 | 30 |
+| app\src\main\res\layout | 1 | 14 | 0 | 2 | 16 |
+| app\src\main\res\mipmap-anydpi-v26 | 4 | 20 | 0 | 0 | 20 |
+| app\src\main\res\values | 4 | 29 | 5 | 0 | 34 |
+| app\src\main\res\values-night | 1 | 11 | 5 | 0 | 16 |
+| app\src\test | 1 | 9 | 5 | 3 | 17 |
+| app\src\test\java | 1 | 9 | 5 | 3 | 17 |
+| app\src\test\java\com | 1 | 9 | 5 | 3 | 17 |
+| app\src\test\java\com\arshsaxena | 1 | 9 | 5 | 3 | 17 |
+| app\src\test\java\com\arshsaxena\github | 1 | 9 | 5 | 3 | 17 |
+| app\src\test\java\com\arshsaxena\github\io | 1 | 9 | 5 | 3 | 17 |
+| gradle | 1 | 5 | 1 | 1 | 7 |
+| gradle\wrapper | 1 | 5 | 1 | 1 | 7 |
+
+[details](details.md)
--- app/src/main/java/com/arshsaxena/github/io/MainActivity.java
+++ app/src/main/java/com/arshsaxena/github/io/MainActivity.java
@@ -2,8 +2,13 @@ package com.arshsaxena.github.io;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -19,27 +24,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FirebaseMessaging.getInstance().subscribeToTopic("notifications");
 
-        myWebView = (WebView)findViewById(R.id.webView);
-        WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        myWebView.loadUrl("https://arshsaxena.github.io");
-        myWebView.setWebViewClient(new WebViewClient());
-        if (savedInstanceState != null)
-        {  ((WebView) findViewById(R.id.webView)).restoreState(savedInstanceState.getBundle("webViewState"));}
+        if (savedInstanceState != null){
+            ((WebView) findViewById(R.id.webView)).restoreState(savedInstanceState.getBundle("webViewState"));
+        }
         else {
             myWebView = (WebView)findViewById(R.id.webView);
-            load();
+            myWebView = (WebView)findViewById(R.id.webView);
+            WebSettings webSettings = myWebView.getSettings();
+            webSettings.setLoadsImagesAutomatically(true);
+            webSettings.setJavaScriptEnabled(true);
+            myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+            webSettings.setAllowContentAccess(true);
+            webSettings.setAllowFileAccess(true);
+            webSettings.setAppCacheEnabled(true);
+            webSettings.setDomStorageEnabled(true);
+            webSettings.setUseWideViewPort(true);
+            myWebView.loadUrl("https://arshsaxena.github.io/");
+            myWebView.setWebViewClient(new MyBrowser(){
+                public void onReceivedError(WebView myWebView, int i, String s, String s1){
+                    myWebView.loadUrl("file:///android_asset/404.html");
+                }
+
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView myWebView, String url) {
+                    if (url == null || url.startsWith("http://") || url.startsWith("https://"))
+                        return false;
+                    try {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        myWebView.getContext().startActivity(intent);
+                        return true;
+                    }
+                    catch (Exception e) {
+                        Log.i(TAG, "shouldOverrideUrlLoading Exception:" + e);
+                        return true;
+                    }
+                }
+            });
         }
     }
 
-    public void load() {
-        myWebView=(WebView)findViewById(R.id.webView);
-        myWebView.setWebViewClient(new MyBrowser());
-        myWebView.getSettings().setLoadsImagesAutomatically(true);
-        myWebView.getSettings().setJavaScriptEnabled(true);
-        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        myWebView.loadUrl("https://arshsaxena.github.io");
-    }
+    private static final String TAG = "MainActivity";
 
     private class MyBrowser extends WebViewClient {
         @Override
